b706542a66ebdb5a3b66327254d9c882cfb26683
Mannheim Zsolt <mannheim.zsolt@maileon.hu>
2021-07-29 12:06:48 +0200

    Resubscribe with same email address fix

--- Xqueue/Maileon/Controller/Subscriber.php
+++ Xqueue/Maileon/Controller/Subscriber.php
@@ -13,16 +13,25 @@ class Subscriber
     protected $storeManager;
 
     /**
+     * Subscriber factory
+     *
+     * @var SubscriberFactory
+     */
+    protected $subscriberFactory;
+
+    /**
      * Plugin constructor.
      *
      * @param \Magento\Framework\App\Request\Http $request
      */
     public function __construct(
         \Magento\Framework\App\Request\Http $request,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory
     ) {
         $this->request = $request;
         $this->storeManager = $storeManager;
+        $this->subscriberFactory = $subscriberFactory;
     }
 
     public function afterExecute(\Magento\Newsletter\Controller\Subscriber\NewAction $subject, $result)
@@ -64,68 +73,72 @@ class Subscriber
 
         if ($module_enabled == 'yes') {
             if (!empty($email)) {
-                if (!empty($apikey) || !empty($permission)) {
-                    $params = $this->request->getParams();
-                    $custom_fields = array(
-                        'magento_storeview_id' => $storeId,
-                        'magento_source' => 'newsletter'
-                    );
-                    $standard_fields = array();
+                if (!$this->emailIsExists($email)) {
+                    if (!empty($apikey) || !empty($permission)) {
+                        $params = $this->request->getParams();
+                        $custom_fields = array(
+                            'magento_storeview_id' => $storeId,
+                            'magento_source' => 'newsletter'
+                        );
+                        $standard_fields = array();
 
-                    if (!empty($params)) {
-                        foreach ($params as $key => $value) {
-                            $exploded_key = explode('_', $key);
-                            
-                            if ($exploded_key[0] == 'standard') {
-                                $standard_fields[$exploded_key[1]] = $value;
-                            }
+                        if (!empty($params)) {
+                            foreach ($params as $key => $value) {
+                                $exploded_key = explode('_', $key);
+                                
+                                if ($exploded_key[0] == 'standard') {
+                                    $standard_fields[$exploded_key[1]] = $value;
+                                }
 
-                            if ($exploded_key[0] == 'custom') {
-                                $custom_fields[$exploded_key[1]] = $value;
+                                if ($exploded_key[0] == 'custom') {
+                                    $custom_fields[$exploded_key[1]] = $value;
+                                }
                             }
                         }
-                    }
 
-                    $maileon_config = array(
-                        'BASE_URI' => 'https://api.maileon.com/1.0',
-                        'API_KEY' => $apikey,
-                        'TIMEOUT' => 15
-                    );
+                        $maileon_config = array(
+                            'BASE_URI' => 'https://api.maileon.com/1.0',
+                            'API_KEY' => $apikey,
+                            'TIMEOUT' => 15
+                        );
 
-                    $ping_service = new PingService($maileon_config);
-                    $response = $ping_service->pingGet();
-                    $ping_result = $response->getStatusCode();
+                        $ping_service = new PingService($maileon_config);
+                        $response = $ping_service->pingGet();
+                        $ping_result = $response->getStatusCode();
 
-                    if ($ping_result == 401) {
-                        $logger->debug('Maileon API key is wrong!');
+                        if ($ping_result == 401) {
+                            $logger->debug('Maileon API key is wrong!');
+                        } else {
+                            try {
+                                $sync = new ContactCreate(
+                                    $apikey,
+                                    $email,
+                                    $permission,
+                                    $doiprocess,
+                                    $doiplusprocess,
+                                    $doikey,
+                                    $print_curl
+                                );
+                                
+                                $response = $sync->makeMalieonContact(null, $standard_fields, $custom_fields);
+
+                                if ($response) {
+                                    $logger->info('Contact subscribe Done!');
+                                } else {
+                                    $logger->debug('Contact subscribe Failed!');
+                                }
+                            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                                $this->messageManager->addException(
+                                    $e,
+                                    __('There was a problem with the Maileon subscription: %1', $e->getMessage())
+                                );
+                            }
+                        }
                     } else {
-                        try {
-                            $sync = new ContactCreate(
-                                $apikey,
-                                $email,
-                                $permission,
-                                $doiprocess,
-                                $doiplusprocess,
-                                $doikey,
-                                $print_curl
-                            );
-                            
-                            $response = $sync->makeMalieonContact(null, $standard_fields, $custom_fields);
-
-                            if ($response) {
-                                $logger->info('Contact subscribe Done!');
-                            } else {
-                                $logger->debug('Contact subscribe Failed!');
-                            }
-                        } catch (\Magento\Framework\Exception\LocalizedException $e) {
-                            $this->messageManager->addException(
-                                $e,
-                                __('There was a problem with the Maileon subscription: %1', $e->getMessage())
-                            );
-                        }
+                        $logger->debug('Maileon API key or permission settings is empty!');
                     }
                 } else {
-                    $logger->debug('Maileon API key or permission settings is empty!');
+                    $logger->debug('Subscriber already exists!');
                 }
             } else {
                 $logger->debug('Email field is empty!');
@@ -133,5 +146,18 @@ class Subscriber
         } else {
             $logger->info('Newsletter module inactive!');
         }
+    }
+
+    private function emailIsExists(string $email)
+    {
+        $subscriber = $this->subscriberFactory->create()->loadByEmail($email);
+
+        if ($subscriber->getId()
+            && (int) $subscriber->getSubscriberStatus() === \Magento\Newsletter\Model\Subscriber::STATUS_SUBSCRIBED
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
